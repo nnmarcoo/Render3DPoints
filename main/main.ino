736d15d3128fa93436cc047ebea370ac053f333e
Marco <marcosteam7@gmail.com>
2024-01-02 13:43:19 -0500

    Readable var name

--- main/main.ino
+++ main/main.ino
@@ -10,20 +10,19 @@
  
 //U8G2_SSD1306_128X64_ALT0_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);                                     // SSD1306 and SSD1308Z are compatible
 //U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // Low spped I2C
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    // Low spped I2C Hybrid
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C disp(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    // Low spped I2C Hybrid
 
  
 void setup(void) {
-  u8g2.begin();
-  Serial.begin(115200); // Set Serial baud rate to 115200
+  disp.begin();
 }
  
 void loop(void) {
   static int posX = 0;
   static int add = 1;
-  u8g2.clearBuffer();
+  disp.clearBuffer();
 
-  u8g2.drawBox(posX, 24, 16, 16);
+  disp.drawBox(posX, 24, 16, 16);
 
   posX+=add;
   if (posX > 128-16)
@@ -31,5 +30,5 @@ void loop(void) {
   if (posX < 0)
     add = 1;
 
-  u8g2.sendBuffer();
+  disp.sendBuffer();
 }
