131ec8f3e469dd643a2e05bd2cad88c6e0e486d3
Marco <marcosteam7@gmail.com>
2024-01-01 19:02:54 -0500

    White screen

--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,9 @@
-	
 #include "Arduino_SensorKit.h"
  
 void setup() {
   Oled.begin();
-  Oled.setFlipMode(true); // Sets the rotation of the screen
+  Oled.fillDisplay();
 }
  
 void loop() {
-  int random_value = random(0, 1000);   //read value from A0
- 
-  Oled.setFont(u8x8_font_chroma48medium8_r); 
-  Oled.setCursor(0, 33);    // Set the Coordinates 
-  Oled.print("Analog Value:");   
-  Oled.print(random_value); // Print the Values  
-  Oled.refreshDisplay();    // Update the Display 
-  //delay(1000);
 }
