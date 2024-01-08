5f59ba6be98d5f2e2b350d4d6aa70788be2c95e0
Marco <marcosteam7@gmail.com>
2024-01-08 15:29:21 -0500

    Update PerspectiveCamera.h

--- src/main/PerspectiveCamera.h
+++ src/main/PerspectiveCamera.h
@@ -1,5 +1,14 @@
 #pragma once
 
+typedef struct { float x, y, z; } 
+3DPoint;
+
+typedef struct { float x, y; } 
+2DPoint;
+
+typedef struct { int n1, n2; } 
+edge;
+
 class PerspectiveCamera {
 
 public:
@@ -9,8 +18,9 @@ public:
     inline float GetFOV() { return FOV; }
 
 
+
 private:
     float FOV;
-
+    float roll;
 
 };
