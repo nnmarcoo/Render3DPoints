ab4d7a5bc5f12d085b46657cdc4e8e9824a38afa
Marco <marcosteam7@gmail.com>
2024-01-09 18:32:43 -0500

    Cleanup

--- main/Renderer.h
+++ main/Renderer.h
@@ -6,7 +6,7 @@
 class Renderer {
 
 public:
-    Renderer(Camera camera, std::vector<point3D> points, std::vector<edge> edges) : camera(camera), points(points), edges(edges) {};
+    Renderer(Camera camera, std::vector<Point3D> points, std::vector<Edge> edges) : camera(camera), points(points), edges(edges) {};
 
     void Render();
 
@@ -14,7 +14,7 @@ private:
     Camera camera;
     float DeltaTime = 0.0f; // What does this do
 
-    std::vector<point3D> points;
-    std::vector<edge> edges;
+    std::vector<Point3D> points;
+    std::vector<Edge> edges;
 
 };
--- main/main.ino
+++ main/main.ino
@@ -19,7 +19,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/
 std::vector<Point3D> points;
 std::vector<Edge> edges;
 
-PerspectiveCamera camera(60.0f);
+PerspectiveCamera camera;
 Renderer renderer(camera, points, edges);
 
  
