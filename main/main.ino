b594532e40da05cf6b512f11ee3a2739fbbd91aa
Marco <marcosteam7@gmail.com>
2024-01-14 15:44:36 -0500

    Read from serial

--- main/main.ino
+++ main/main.ino
@@ -1,32 +1,41 @@
 #include <Arduino.h>
 #include <U8g2lib.h> // Should this be in the project directory?
 
+
 #include "ExampleObjects.h"
 #include "PerspectiveCamera.h"
 #include "OrthographicCamera.h"
 #include "Renderer.h"
+
+#define SCALE 20
  
 //U8G2_SSD1306_128X64_ALT0_F_HW_I2C display(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);                                     // SSD1306 and SSD1308Z are compatible
 //U8G2_SSD1306_128X64_NONAME_F_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // Low spped I2C
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    // Low spped I2C Hybrid
 
-std::vector<Object*> objects = {&cube, &pyramid};
+std::vector<Object*> objects = {&cube};
 
-PerspectiveCamera camera(6.0f);
+PerspectiveCamera camera;
 Renderer renderer(display, camera, objects);
 
 void setup(void) {
   display.begin();
-
-  renderer.SetScale(10);
+  Serial.begin(2000000);
+  
+  renderer.SetScale(SCALE);
   renderer.SetOrigin(64, 32);
-
-  pyramid.Translate(0, -2.0f);
-  cube.Translate(0, 2.0f);
+  delay(5000);
 }
  
 float r;
+unsigned int frame = 0;
 void loop(void) {
-  renderer.SetRotation(r+=.015);
+  r += 0.015;
+  renderer.SetRotation(r);
+  //cube.Translate(0, 2.5*cos(r*5) / SCALE);
   renderer.Render();
+  if (frame < 150)
+    display.writeBufferXBM(Serial);
+
+  frame++;
 }
---
+++ serial/serial.py
@@ -0,0 +1,33 @@
+import serial
+
+def flipxbm(xbm):
+    output = ''
+    i = 0
+    while i < len(xbm):
+        if xbm[i] == 'x' and xbm[i:i+3] != 'xbm':
+            output += str(hex(int(xbm[i+1:i+3], 16) ^ (2 ** 8 - 1)))
+            i += 3 
+        else:
+            output += xbm[i]
+            i+=1
+    return output
+
+def readserialbitmap(comport, baudrate):
+    ser = serial.Serial(comport, baudrate, timeout=0.1) # 1/timeout is the frequency at which the port is read
+    bitmap = ''
+    frame = 0
+    while True:
+        data = ser.readline().decode().strip()
+        if data:
+            bitmap += data + '\n'
+            if "}" in data:
+                print('Captured frame: ', frame)
+
+                with open(str(frame) + '.xbm', 'w') as file:
+                    file.write(flipxbm(bitmap))
+
+                bitmap = ''
+                frame+=1
+
+if __name__ == '__main__':
+    readserialbitmap('COM7', 2000000)
