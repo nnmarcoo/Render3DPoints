6eb404ee3cd32bd5587ee05d27ee6c2caa48b93e
Marco <marcosteam7@gmail.com>
2024-01-14 16:36:42 -0500

    Convert to gif

--- serial/serial.py
+++ serial/serial.py
@@ -1,7 +1,10 @@
 import serial
+from PIL import Image
+from io import BytesIO
 
 PORT = 'COM7'
 BAUD = 2000000
+FRAMES = 10
 
 
 def flipxbm(xbm):
@@ -16,10 +19,12 @@ def flipxbm(xbm):
             i+=1
     return output
 
-def readserialbitmap(comport, baudrate):
-    ser = serial.Serial(comport, baudrate, timeout=0.1) # 1/timeout is the frequency at which the port is read
+def getbitmaps():
+    
+    ser = serial.Serial(PORT, BAUD, timeout=0.1) # 1/timeout is the frequency at which the port is read
     bitmap = ''
-    frame = 0
+    bitmaps = []
+    frame = 1
 
     print('Waiting for data...')
 
@@ -30,11 +35,26 @@ def readserialbitmap(comport, baudrate):
             if "}" in data:
                 print('Captured frame: ', frame)
 
-                with open(str(frame) + '.xbm', 'w') as file:
-                    file.write(flipxbm(bitmap))
+                bitmaps.append(flipxbm(bitmap))
 
                 bitmap = ''
                 frame+=1
+        if frame >= FRAMES:
+            return bitmaps
+
+def convertgif(bitmaps, output):
+    images = []
+
+    for bitmap in bitmaps:
+        image = Image.open(BytesIO(bitmap))
+        print(str(image))
+        images.append(image)
+
+
+    images[0].save(output, save_all=True, append_images=images[1:], duration=100, loop=0)
+
 
 if __name__ == '__main__':
-    readserialbitmap(PORT, BAUD)
+    bitmaps = getbitmaps()
+    convertgif(bitmaps, 'test.gif')
+    
