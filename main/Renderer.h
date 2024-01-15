fb4163f1b8ebbec0f6091f433d802ff7179bf8e1
Marco <marcosteam7@gmail.com>
2024-01-15 12:45:17 -0500

    Removed Renderer.cpp

--- main/Renderer.h
+++ main/Renderer.h
@@ -2,17 +2,35 @@
 #include <vector>
 #include "Object.h"
 
-class U8G2;
+#include <U8g2lib.h>
 
 class Renderer {
 public:
   Renderer(U8G2 &display, Camera &camera, std::vector<Object*> objects) 
           : display(display), camera(camera), objects(objects) {};
 
-  void Render();
-
   void SetScale(float nscale)  { scale = nscale;  }
   void SetOrigin(int x, int y) { origin = {x,y};  }
+
+  void Render() {
+  display.clearBuffer();
+
+  for (auto obj : objects) {
+    std::vector<Point> points = obj->GetPoints();
+    std::vector<Edge>  edges  = obj->GetEdges();
+
+    for (auto edge : edges) {
+      Point n1 = camera.Project(points[edge.p1]);
+      Point n2 = camera.Project(points[edge.p2]);
+
+      display.drawLine(scale * n1[0] + origin[0],
+                      -scale * n1[1] + origin[1],
+                       scale * n2[0] + origin[0],
+                      -scale * n2[1] + origin[1]);
+    }
+  }
+  display.sendBuffer();
+}
 
 private:
   U8G2 &display;
