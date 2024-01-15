1422a218e475c526560e330aaac494e403f9a1d5
Marco <marcosteam7@gmail.com>
2024-01-15 00:58:52 -0500

    Removed unused objects

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h> // Should this be in the project directory?
 
-
 #include "ExampleObjects.h"
 #include "PerspectiveCamera.h"
 #include "OrthographicCamera.h"
@@ -9,9 +8,7 @@
 
 #define SCALE 20
  
-//U8G2_SSD1306_128X64_ALT0_F_HW_I2C display(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);                                     // SSD1306 and SSD1308Z are compatible
-//U8G2_SSD1306_128X64_NONAME_F_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // Low spped I2C
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    // Low spped I2C Hybrid
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 
 std::vector<Object*> objects = {&cube};
 
@@ -34,7 +31,7 @@ void loop(void) {
   cube.Translate(0, 2.5*cos(r*5) / SCALE);
   renderer.Render();
   //if (frame < 500)
-   // display.writeBufferXBM(Serial);
+    //display.writeBufferXBM(Serial);
 
   frame++;
 }
