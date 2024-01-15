30091cab6919520212bce3742844fa230e7e0bf4
Marco <marcosteam7@gmail.com>
2024-01-15 05:03:40 -0500

    TODO

--- main/Renderer.h
+++ main/Renderer.h
@@ -13,20 +13,16 @@ public:
 
   void SetScale(float nscale)  { scale = nscale;  }
   void SetOrigin(int x, int y) { origin = {x,y};  }
-  void SetRotation(float nrot) { rotation = nrot; }
 
 private:
   U8G2 &display;
   Camera &camera;
   Point origin;
 
-  // TODO: These attributes should probably exist for individual objects, not the entire renderer
+  // TODO: Add rotation ??
   float scale = 1.0f;
   float rotation = 0.0f;
 
   std::vector<Object*> objects;
 
-  Point RotateX(Point point);
-  Point RotateY(Point point);
-  Point RotateZ(Point point);
 };
