7ebb5b0d63900e662659fb4284f23d09c97f26f0
Marco <marcosteam7@gmail.com>
2024-01-15 15:18:25 -0500

    Broken 4D projection

--- main/main.ino
+++ main/main.ino
@@ -2,17 +2,18 @@
 #include <U8g2lib.h> // Should this be in the project directory?
 
 #include "ExampleObjects.h"
-#include "PerspectiveCamera3D.h"
 #include "OrthographicCamera.h"
+#include "PerspectiveCamera3D.h"
+#include "PerspectiveCamera4D.h"
 #include "Renderer.h"
 
 #define SCALE 20
  
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 
-std::vector<Object*> objects = {&cube};
+std::vector<Object*> objects = {&tesseract};
 
-PerspectiveCamera3D camera;
+PerspectiveCamera4D camera(15.0f, 0.5f);
 Renderer renderer(display, camera, objects);
 
 void setup(void) {
@@ -21,9 +22,6 @@ void setup(void) {
   
   renderer.SetScale(SCALE);
   renderer.SetOrigin(64, 32);
-
-  cube.Rotate(1, 2, 1.0f);
-  cube.Rotate(0, 2, -2.0f);
 }
  
 float r;
@@ -31,10 +29,11 @@ unsigned int frame = 0;
 void loop(void) {
   r += 0.015f;
   //renderer.SetRotation(r);
-  cube.Translate(0, 2.5*cos(r*5) / SCALE);
-  cube.Rotate(1, 2, 2.5*cos(r*5) / SCALE);
-  cube.Rotate(0, 2, 2.5*cos(r*5) / SCALE);
-  cube.Rotate(0, 1, 2.5*sin(r*5) / SCALE);
+  //tesseract.Translate(0, 2.5*cos(r*5) / SCALE);
+  //tesseract.Rotate(1, 2, 2.5*cos(r*5) / SCALE);
+  //tesseract.Rotate(0, 1, 0.015f);
+  tesseract.Rotate(1, 3, 0.015f);
+  //tesseract.Rotate(0, 1, 2.5*sin(r*5) / SCALE);
   renderer.Render();
   //if (frame < 150)
     //display.writeBufferXBM(Serial);
