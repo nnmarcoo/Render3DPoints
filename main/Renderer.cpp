7ac8a2f239814277706344454f846ed9f7c49437
Marco <marcosteam7@gmail.com>
2024-01-14 23:58:09 -0500

    Cleanup

--- main/Renderer.cpp
+++ main/Renderer.cpp
@@ -7,7 +7,7 @@ void Renderer::Render() {
 
   for (auto obj : objects) {
     std::vector<Point> points = obj->GetPoints();
-    std::vector<Edge> edges = obj->GetEdges();
+    std::vector<Edge>  edges  = obj->GetEdges();
 
     for (auto edge : edges) {
       Point n1 = camera.Project(RotateX(RotateZ(RotateY(points[edge.p1]))));
@@ -15,7 +15,7 @@ void Renderer::Render() {
 
       display.drawLine(scale * n1[0] + origin[0],
                       -scale * n1[1] + origin[1],
-                       scale * n2[0] + origin[0], 
+                       scale * n2[0] + origin[0],
                       -scale * n2[1] + origin[1]);
     }
   }
