42cb6cfcd8be6f9595af4ea557d104f2fd2fd1c0
Marco <marcosteam7@gmail.com>
2024-01-16 15:03:22 -0500

    Fixed min distance to expected normalized cords

--- main/PerspectiveCamera4D.h
+++ main/PerspectiveCamera4D.h
@@ -3,8 +3,8 @@
 
 class PerspectiveCamera4D : public PerspectiveCamera3D {
 public:
-  PerspectiveCamera4D() : PerspectiveCamera3D(), distance(0.1f) {}
-  PerspectiveCamera4D(float fov) : PerspectiveCamera3D(fov), distance(0.1f) {}
+  PerspectiveCamera4D() : PerspectiveCamera3D(), distance(2.0f) {}
+  PerspectiveCamera4D(float fov) : PerspectiveCamera3D(fov), distance(2.0f) {}
   PerspectiveCamera4D(float fov, float distance) : PerspectiveCamera3D(fov), distance(distance) {}
 
   void SetDistance(float ndistance) { distance = ndistance; };
