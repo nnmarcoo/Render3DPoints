1c3e57db1bfec2b50ea350064c9c5cf8533fefde
Marco <marcosteam7@gmail.com>
2024-01-16 15:14:04 -0500

    Update OrthographicCamera.h

--- main/OrthographicCamera.h
+++ main/OrthographicCamera.h
@@ -3,6 +3,9 @@
 
 class OrthographicCamera : public Camera {
 public:
+  PerspectiveCamera3D() : Camera() {}
+  PerspectiveCamera3D(float fov) : Camera(fov) {}
+  
   virtual Point Project(Point point) const override {
     return { point[0], point[1] };
   }
