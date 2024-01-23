1d638491465525bac829813b1851f3110b8b6dee
Marco <marcosteam7@gmail.com>
2024-01-23 02:18:52 -0500

    Completed PerspectiveCameraND

--- main/PerspectiveCameraND.h
+++ main/PerspectiveCameraND.h
@@ -1,25 +1,20 @@
 #pragma once
 #include "PerspectiveCamera3D.h"
 
-// TODO: Implement
-
 class PerspectiveCameraND : public PerspectiveCamera3D {
 public:
-  PerspectiveCameraND() : PerspectiveCamera3D(), distance(2.0f), dimensions(3) {}
-  PerspectiveCameraND(float fov) : PerspectiveCamera3D(fov), distance(2.0f), dimensions(3) {}
+  PerspectiveCameraND() : PerspectiveCamera3D(), distance(2.0f) {}
+  PerspectiveCameraND(float fov) : PerspectiveCamera3D(fov), distance(2.0f) {}
   PerspectiveCameraND(float fov, float distance) : PerspectiveCamera3D(fov), distance(distance) {}
-  PerspectiveCameraND(float fov, float distance, size_t d) : PerspectiveCamera3D(fov), distance(distance) { if (d < 4) dimensions = 3; else dimensions = d;}
 
   void  SetDistance(float ndistance) { distance = ndistance; };
   float GetDistance() const { return distance; }
 
   virtual Point Project(Point point) const override {
-    Point pos = point.GetSize();
-    
-    while (point.GetSize() > 3) {
-      float w = 1 / (distance - point[point.GetSize()-1]);
-      for (int i = 0; i < point.GetSize()) {
-        point[i] * w;
+    for (int i = point.GetSize(); i > 3; i--) {
+      float w = 1 / (distance - point[i-1]);
+      for (int j = 0; j < i-1; j++) {
+        point[j] *= w;
       }
     }
     return PerspectiveCamera3D::Project(point);
@@ -27,5 +22,4 @@ public:
 
 private:
   float distance;
-  size_t dimensions;
 };
